d754e8534eb42eb7f45a604aa9123a75ff029a25
shiyinxi <shiyinxi@outlook.com>
2024-04-28 13:09:06 -0700

    Updates to structure, create main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-window.addEventLostener('DOMContentLoaded', (event)=>{
+window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
@@ -11,7 +11,7 @@ const getVisitCount = () => {
     }).then(response =>{
         console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innerText = count;        
+        document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
     });
